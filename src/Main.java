173a1728e0727629754f3b12c92fe02fb0d73892
Atticus Blanco <atticusbrujx@gmail.com>
2023-10-26 23:53:17 -0700

    Part 1 >>>NONOPERATIONAL
    >>>MODIFICATIONS
    --Main.java
      -DECLARED instance variables userString and filePath and skipWs
      -CREATED Scanner object s
      -CREATED while loop to handle creation of File object
      -CREATED while tool to handle skip whitespace option
      -CREATED FileStats object
    
    >>>DEFICITS
    --FileStats.java
      -BUILD FileStats constructor
      -BUILD countWords method
      -BUILD removeSpaces method
      -BUILD countChars method
      -BUILD read method + overload
    
    >>>BUGS
    --
    
    >>>THROWS
    --C:\Users\Gnomatic\IdeaProjects\Java-Lab-008\src\Main.java
      -Warning:(3, 1) Unused import statement
      -Warning:(4, 1) Unused import statement
      -Warning:(9, 27) Field can be converted to a local variable
      -Warning:(21, 5) 'while' statement does not loop
      -Warning:(25, 14) Variable 'file' is never used
      -Warning:(37, 24) Method invocation 'equalsIgnoreCase' will produce 'NullPointerException'
      -Warning:(53, 27) Variable 'fileStats' is never used
      -Error:(53, 53) Cannot resolve symbol 'file'

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,51 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static String userString;
+    private static String filePath;
+    private static boolean skipWs;
+
     public static void main(String[] args) {
-        // Create a scanner object
 
-        // Write a loop that will ask the user to enter a file path to gather stats on,
-        // and continue until "Q" is entered.
+    // Create a scanner object
+    Scanner s = new Scanner(System.in);
 
-            // Reference Java-Assignment-003 to see how to use the java.nio libraries to turn a String path into a File
 
-            // Ask the user if they would like to skip whitespace
+    // Write a loop that will ask the user to enter a file path to gather stats on,
+    // and continue until "Q" is entered.
+    while (!(filePath.equalsIgnoreCase("Q")) ) {
 
-            // Create a variable called skipWs that stores the user's response as a boolean
+        //Copied from Assignment 3
+        filePath = s.nextLine();                               //user input
+        File file = new File(filePath);                               //File object created
 
-            /*
-             * Within this try/catch block, which is used to handle possible errors thrown by the code in the try block,
-             * write code to get the line, word, and character count of the File object created above!
-             */
+        break;
+    }
+
+
+    // Ask the user if they would like to skip whitespace
+    // Create a variable called skipWs that stores the user's response as a boolean
+    System.out.println("Skip whitespace? Y/N");
+    userString = s.nextLine();
+
+    while (userString == null) {
+        if (userString.equalsIgnoreCase("N")) {
+            skipWs = false;
+        } else if (userString.equalsIgnoreCase("Y")) {
+            skipWs = true;
+        } else {
+            System.out.println("Please enter \"Y\" or \"N\"\n");
+        }
+    }
+
+
+        /*
+         * Within this try/catch block, which is used to handle possible errors thrown by the code in the try block,
+         * write code to get the line, word, and character count of the File object created above!
+         */
             try {
                 // You will need to create a FileStats object by passing it the File object and your skipWs variable as args
+                FileStats fileStats = new FileStats(file, skipWs);
 
                 // You will need to call the fs.read method, which you need to implement!
 
@@ -36,6 +63,5 @@ public class Main {
             } catch (IOException e) {
                 System.err.println(e.getMessage());
             }
-
     }
 }
