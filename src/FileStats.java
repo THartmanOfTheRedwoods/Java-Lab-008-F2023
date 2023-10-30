57e02b2b4efa9c774855cb8139783f54369847c0
Atticus Blanco <atticusbrujx@gmail.com>
2023-10-29 18:23:43 -0700

    Part 1 >>>OPERATIONAL
    >>>MODIFICATIONS
    --Main.java
      -CREATED repeatProgram var
      -ENCAPSULATED file names and file paths to a matrix
      -ENCAPSULATED file selection into fileSelection method
      -ENCAPSULATED white space boolean option into whiteSpaceSelection method
      -ENCAPSULATED fileSelection and whiteSpaceSelection methods into launchProgram method
      -CREATED repeatOption method to control if program will repeat based on user input
    
    --FileStats.java
      -MODIFIED output with dividers
      -FIXED read() method: if/else tree to control whitespace output depending on skipWhiteSpace boolean
    
    >>>DEFICITS
    --
    
    >>>BUGS
    --Program will exit after second call of whiteSpaceSelection, at creation of new FileStats object
    
    >>>THROWS
    --C:\Users\Gnomatic\IdeaProjects\Java-Lab-008\src\Main.java
      -Warning:(7, 27) Field can be converted to a local variable
      -Warning:(8, 27) Field can be converted to a local variable
      -Warning:(12, 27) Field can be converted to a local variable
      -Warning:(12, 27) Field 'outputScript' may be 'final'
      -Warning:(13, 28) Field 's' may be 'final'
      -Warning:(36, 13) Switch statement can be replaced with enhanced 'switch'
    --C:\Users\Gnomatic\IdeaProjects\Java-Lab-008\src\FileStats.java
      -Warning:(7, 21) Field 'skipWhiteSpace' may be 'final'
      -Warning:(8, 20) Field can be converted to a local variable
      -Warning:(8, 20) Field 'divider' may be 'final'
      -Warning:(46, 37) There is a more general exception, 'java.io.IOException', in the throws list already.
      -Warning:(54, 31) There is a more general exception, 'java.io.IOException', in the throws list already.
      -Warning:(61, 21) Condition 'line != null' is always 'true'

--- src/FileStats.java
+++ src/FileStats.java
@@ -5,6 +5,7 @@ public class FileStats {
     private int numWords;
     private int numChars;
     private boolean skipWhiteSpace;
+    private String divider = "******************************\n";
     private File f;
 
     public FileStats(File f, boolean skipWhiteSpace) throws FileNotFoundException {
@@ -17,6 +18,7 @@ public class FileStats {
         this.numLines = 0;
         this.numWords = 0;
         this.numChars = 0;
+
     }
 
     private static int countWords(String line) {
@@ -39,6 +41,8 @@ public class FileStats {
         return line.length();
     }
 
+
+
     public void read(File f) throws FileNotFoundException, IOException {
         if( !f.exists() ) {
             throw new FileNotFoundException(String.format("File: %s does not exist.", f.getName()));
@@ -51,11 +55,25 @@ public class FileStats {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(f))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
+                if (numLines == 0) {
+                    System.out.printf("%s%s", divider, divider);
+                }
+                if (line != null) {
+                    if (skipWhiteSpace) {
+                        System.out.println(removeSpaces(line));
+                    } else {
+                        System.out.println(line);
+                    }
+                }
                 numWords += countWords(line);
                 numLines++;
                 numChars += countChars(line, skipWhiteSpace);
                 }
-            }
+            System.out.printf("%s%s", divider, divider);
+
+
+
+        }
         }
 
     public int getNumLines() {
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
-import javax.swing.text.html.Option;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
@@ -11,42 +7,79 @@ public class Main {
     private static String userString;
     private static String filePath;
     private static boolean skipWs;
+    private static boolean repeatProgram;
     private static File file;
-    private static String outputScript = "Stats: lines - %d, words - %d, chars - %d %s";
+    private static String outputScript = "Stats: lines - %d, words - %d, chars - %d %s%n";
+    private static Scanner s = new Scanner(System.in);
 
+    private static final String[][] fileNameAndPath = {
+        {"Test.txt", "resources/TEST.txt"},
+        {"Sisko's Sin", "resources/Sisko's Sin"},
+        {"Janeway's Mission", "resources/Janeway's Mission"},
+        {"Yar's Farewell", "resources/Yar's Farewell"}
+    };
 
-    public static void main(String[] args) {
-    Scanner s = new Scanner(System.in);
-    System.out.println("Enter filepath");
-    filePath = s.nextLine();
-
-        while (!filePath.equalsIgnoreCase("Q")) {
-            file = new File(filePath);
-            break;
+    public static void fileSelection() {
+        System.out.println("--Select a passage--");
+        for (int i = 0; i < fileNameAndPath.length; i++) {
+            System.out.printf("\"%d\": %s%n", i + 1, fileNameAndPath[i][0]);
         }
+        filePath = fileNameAndPath[Integer.parseInt(s.nextLine()) - 1][1];
+        file = new File(filePath);
+    }
+    
+    public static boolean repeatOption() {
+        String input = "";
+        while (!input.equalsIgnoreCase("Y") && !input.equalsIgnoreCase("N")) {
+            System.out.println("Select another text? Y/N");
+            input = s.nextLine();
+            switch (input.toUpperCase()) {
+                case "Y":
+                    repeatProgram = true;
+                    break;
+                case "N":
+                    repeatProgram = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        }
+        return repeatProgram;
+    }
 
-    System.out.println("Skip whitespace? Y/N");
-    userString = s.nextLine();
-
-    while (true) {
-        if (userString.equalsIgnoreCase("N")) {
-            skipWs = false;
-            break;
-        } else if (userString.equalsIgnoreCase("Y")) {
-            skipWs = true;
-            break;
-        } else {
-            System.out.println("Please enter \"Y\" or \"N\"\n");
-            userString = s.nextLine();
+    public static void whiteSpaceSelection() {
+        System.out.println("Skip whitespace? Y/N");
+        userString = s.nextLine();
+        while (true) {
+            if (userString.equalsIgnoreCase("N")) {
+                skipWs = false;
+                break;
+            } else if (userString.equalsIgnoreCase("Y")) {
+                skipWs = true;
+                break;
+            } else {
+                System.out.println("Please enter \"Y\" or \"N\"\n");
+                userString = s.nextLine();
+            }
         }
     }
 
+    public static void launchProgram() {
+        fileSelection();
+        whiteSpaceSelection();
+    }
+
+    public static void main(String[] args) {
+        launchProgram();
             try {
                 FileStats fs = new FileStats(file, skipWs);
                 fs.read(file);
                 System.out.printf(outputScript, fs.getNumLines(), fs.getNumWords(), fs.getNumChars(), fs.getFileName());
             } catch (IOException e) {
                 System.err.println(e.getMessage());
+            }
+            if (repeatOption()) {
+                launchProgram();
             }
     }
 }
